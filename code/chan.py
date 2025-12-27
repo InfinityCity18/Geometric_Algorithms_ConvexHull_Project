94db438d3cc333ac28ea3528737525382d268941
Jakub <kubawlas1@gmail.com>
2025-12-27 05:14:52 +0100

    raw prog

--- code/chan.py
+++ code/chan.py
@@ -1,28 +1,30 @@
 eps = 10**-12
 
 from graham import graham
+from itertools import batched
 
 def det(a, b, c):       
      return (b[0]-a[0])*(c[1]-a[1]) - (b[1]-a[1])*(c[0]-a[0])
 
-def batched_merge_tail(seq, n):
-    batches = [seq[i:i+n] for i in range(0, len(seq), n)]
-    if len(batches) > 1 and len(batches[-1]) < n:
-        batches[-2].extend(batches.pop())
-    return batches
-
-
-def chan(points, m):
+def step_chan(points, m):
     n = len(points)
-    hulls = list(map(print, batched_merge_tail(points, m))) #dzielimy na n / m otoczek, aplikujemy grahama na kazda, jesli n sie nie dzieli przez m to ostatnia jest wieksza
+    hulls = list(map(graham, batched(points, n=m))) #dzielimy na n / m otoczek, aplikujemy grahama na kazda
+    # zakladam ze otoczka grahama dziala poprawnie na 1,2,3 punkty 
 
     p1 = max(points, key=lambda p: p[0]) #najbardziej na prawo
     p0 = (p1[0] + 1.0, p1[1]) 
     #ogolnie to jest aby wybrac punkt w nieskonczonosci, ale tu jest + 1.0 w prawo, idk jak poprawic bo to moze sie popsuc przez precyzje floatow
+    #nie chcialo mi sie myslec nad tym az tyle xd wiec do poprawy
     final_hull = []
     final_hull.append(p1)
 
     for j in range(m):
+        best = points[0] if points[0] != p1 else points[1] #chyba pasi aby pierwszy lepszy punkt xd
+        for i in range(len(hulls)):
+            q = maximized_angle #u trzeba znalezc w hull[i] najlepszy punkt dla hulla ale nie wiem jak to zrobic w log czasie, binsearch? nie widze na razie tego i ide spac xd
+            d = det(p0,)
+
+
 
 
 chan([1,2,3,4,5,6,7,8,9,10], 3)
