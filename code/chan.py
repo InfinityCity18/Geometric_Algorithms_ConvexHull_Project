2bc0a0acb4767d72821f1e3194dff1e44918edc2
Jakub <kubawlas1@gmail.com>
2025-12-28 02:18:45 +0100

    chan cleanup

--- code/chan.py
+++ code/chan.py
@@ -1,9 +1,7 @@
-eps = 10**-12
-
 from graham import graham
 from itertools import batched
-from drawing import draw_hull
-import matplotlib.pyplot as plt
+
+eps = 10**-12
 
 def det(a, b, c):       
      return (b[0]-a[0])*(c[1]-a[1]) - (b[1]-a[1])*(c[0]-a[0])
@@ -11,26 +9,25 @@ def det(a, b, c):
 def dist(p1,p2):
     return (p1[0] - p2[0])**2 + (p1[1]-p2[1])**2
 
-# def turn(p, q, r):
-#     """Returns -1, 0, 1 if p,q,r forms a right, straight, or left turn."""
-#     return det(p,q,r) > eps - det(p,q,r) < -eps
-
 def turn(p, q, r):
     def cmp(a, b):
         return (a > b) - (a < b)
-    """Returns -1, 0, 1 if p,q,r forms a right, straight, or left turn."""
     return cmp((q[0] - p[0])*(r[1] - p[1]) - (r[0] - p[0])*(q[1] - p[1]), 0)
 
-
 def rtangent(hull, p):
-    if p in hull: #jesli jest w otoczce to funkcja zwroci wlasnie ten punkt, czego nie chcemy
+    '''
+    Zwraca punkt otoczki bedacy styczna prawostronna z punktu p
+    
+    :param hull: Otoczka wypukla w postaci listy punktow ukladzie przeciwnym do ruchu wskazowek zegara
+    :param p: Punkt spoza otoczki
+    '''
+
+    #shamelessly ukradzione https://gist.github.com/tixxit/252229
+    if p in hull: #jesli p jest w otoczce to funkcja zwroci wlasnie ten punkt, czego nie chcemy
         hull = hull.copy() #kopiujemy bo nie chcemy modyfikowac oryginalnej otoczki
         hull.remove(p)
+
     TURN_LEFT, TURN_RIGHT = (1, -1)
-    """Return the index of the point in hull that the right tangent line from p
-    to hull touches.
-    """
-    #shamelessly ukradzione https://gist.github.com/tixxit/252229
     l, r = 0, len(hull)
     l_prev = turn(p, hull[0], hull[-1])
     l_next = turn(p, hull[0], hull[(l + 1) % r])
@@ -51,31 +48,16 @@ def rtangent(hull, p):
             l_next = turn(p, hull[l], hull[(l + 1) % len(hull)])
     return hull[l]
 
-# def tangential_binsearch(hull, p0):
-#     left, right = 0, len(hull)
-#     while left < right:
-#         mid = (left + right) // 2
-#         if det(p0, hull[mid], hull[(mid + 1) % len(hull)]) > 0:
-#             right = mid
-#         else:
-#             left = mid + 1
-#     return hull[left % len(hull)]
-
 def step_chan(points, m):
     n = len(points)
     hulls = list(map(graham, map(list, batched(points, n=m)))) #dzielimy na n / m otoczek, aplikujemy grahama na kazda
-    # zakladam ze otoczka grahama dziala poprawnie na 1,2,3 punkty 
-    for hull in hulls:
-        xh, yh = zip(*(hull + [hull[0]]))
-        plt.scatter(xh, yh,color ="red",s=50)
-        plt.plot(xh,yh,color="red")
 
     p1 = max(points, key=lambda p: (p[0], p[1])) #najbardziej na prawo
     last = p1
     L = [last]
 
     for j in range(m):
-        best = points[0] if points[0] != last else points[1] #chyba pasi aby pierwszy lepszy punkt xd
+        best = points[0] if points[0] != last else points[1] 
         for i in range(len(hulls)):
             q = rtangent(hulls[i], last) 
             d = det(last,best,q)
@@ -83,13 +65,10 @@ def step_chan(points, m):
                 best = q
         L.append(best)
         last = best
-        plt.plot(last[0], last[1], 'bo')
         if best == p1:
-            plt.show()
             L.pop()  # usuwamy powtorzony punkt startowy
             return L
         
-    plt.show()
     return None
 
 def chan(points):
