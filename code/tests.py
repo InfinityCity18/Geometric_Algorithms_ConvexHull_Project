67594e8bf8dc72f2f1b54fa8bbe89ec4b7bfd735
Jakub <kubawlas1@gmail.com>
2025-12-28 04:33:20 +0100

    ?

--- code/tests.py
+++ code/tests.py
@@ -1,7 +1,7 @@
 import numpy as np
 from drawing import draw_points, draw_hull
 
-np.random.seed(117) #prosze ziarnowac swoje generatory
+np.random.seed(19)
 
 def generate_uniform_points(left=-100, right=100, n=100):
     return [(np.random.uniform(left, right), np.random.uniform(left, right)) for _ in range(n)]
@@ -19,12 +19,12 @@ from jarvis import *
 from monochain import *
 from quickhull import *
 from divide_and_conquer import *
+from chan import *
 print("convex hull points jarvis: ", len(jarvis(testset)))
+draw_hull(testset, jarvis(testset))
+draw_hull(testset, quickhull(testset))
 print("convex hull points graham: ", len(graham(testset)))
 print("convex hull points monochain: ", len(monochain(testset)))
 print("convex hull points quickhull: ", len(quickhull(testset)))
 print("convex hull points divide and conquer: ", len(divide_and_conquer(testset)))
-
-
-
-
+print("convex hull points chan:", len(chan(testset)))
