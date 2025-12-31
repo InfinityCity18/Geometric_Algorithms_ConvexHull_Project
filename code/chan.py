d36bb5afc4d22c1c718f40b1d908a8f67c07fd0c
Jakub <kubawlas1@gmail.com>
2025-12-31 03:18:50 +0100

    chan fix

--- code/chan.py
+++ code/chan.py
@@ -1,5 +1,6 @@
 from graham import graham
 from itertools import batched
+import matplotlib.pyplot as plt
 
 eps = 10**-12
 
@@ -10,9 +11,16 @@ def dist(p1,p2):
     return (p1[0] - p2[0])**2 + (p1[1]-p2[1])**2
 
 def turn(p, q, r):
-    def cmp(a, b):
-        return (a > b) - (a < b)
-    return cmp((q[0] - p[0])*(r[1] - p[1]) - (r[0] - p[0])*(q[1] - p[1]), 0)
+    a = (q[0] - p[0])*(r[1] - p[1]) - (r[0] - p[0])*(q[1] - p[1])
+    if abs(a) < eps:
+        return 0
+    elif a > 0:
+        return 1
+    else:
+        return -1
+    # def cmp(a, b):
+    #     return (a > b) - (a < b)
+    # return cmp((q[0] - p[0])*(r[1] - p[1]) - (r[0] - p[0])*(q[1] - p[1]), eps)
 
 def rtangent(hull, p):
     '''
@@ -23,30 +31,51 @@ def rtangent(hull, p):
     '''
 
     #shamelessly ukradzione https://gist.github.com/tixxit/252229
-    if p in hull: #jesli p jest w otoczce to funkcja zwroci wlasnie ten punkt, czego nie chcemy
+    if len(hull) == 1:
+        return hull[0]
+    elif p in hull: #jesli p jest w otoczce to funkcja zwroci wlasnie ten punkt, czego nie chcemy
         hull = hull.copy() #kopiujemy bo nie chcemy modyfikowac oryginalnej otoczki
         hull.remove(p)
+        if len(hull) == 1:
+            return hull[0]
 
-    TURN_LEFT, TURN_RIGHT = (1, -1)
-    l, r = 0, len(hull)
+    TURN_LEFT, TURN_RIGHT, COLL = (1, -1, 0)
+    n = len(hull)
+    l, r = 0, n - 1
+
     l_prev = turn(p, hull[0], hull[-1])
-    l_next = turn(p, hull[0], hull[(l + 1) % r])
-    while l < r:
+    l_next = turn(p, hull[0], hull[1])
+
+    if l_next == 0:
+        i = 0
+        while turn(p, hull[i], hull[(i + 1) % n]) == 0:
+            i = (i + 1) % n
+        return hull[i]
+
+    while l <= r:
         c = (l + r) // 2
-        c_prev = turn(p, hull[c], hull[(c - 1) % len(hull)])
-        c_next = turn(p, hull[c], hull[(c + 1) % len(hull)])
+
+        c_prev = turn(p, hull[c], hull[(c - 1) % n])
+        c_next = turn(p, hull[c], hull[(c + 1) % n])
         c_side = turn(p, hull[l], hull[c])
+
         if c_prev != TURN_RIGHT and c_next != TURN_RIGHT:
-            return hull[c]
-        elif c_side == TURN_LEFT and (l_next == TURN_RIGHT or
-                                      l_prev == l_next) or \
-                c_side == TURN_RIGHT and c_prev == TURN_RIGHT:
-            r = c               # Tangent touches left chain
+            i = c
+            while turn(p, hull[i], hull[(i + 1) % n]) == 0:
+                i = (i + 1) % n
+            return hull[i]
+
+        elif (c_side == TURN_LEFT and
+            (l_next == TURN_RIGHT or l_prev == l_next)) or \
+        (c_side == TURN_RIGHT and c_prev == TURN_RIGHT):
+            r = c - 1
         else:
-            l = c + 1           # Tangent touches right chain
-            l_prev = -c_next    # Switch sides
-            l_next = turn(p, hull[l], hull[(l + 1) % len(hull)])
-    return hull[l]
+            l = c + 1
+            if l < n:
+                l_prev = -c_next
+                l_next = turn(p, hull[l], hull[(l + 1) % n])
+
+    return hull[l % n]
 
 def step_chan(points, m):
     n = len(points)
@@ -55,6 +84,11 @@ def step_chan(points, m):
     p1 = max(points, key=lambda p: (p[0], p[1])) #najbardziej na prawo
     last = p1
     L = [last]
+
+    for hull in hulls:
+        xh, yh = zip(*(hull + [hull[0]]))
+        plt.scatter(xh, yh,color ="red",s=50)
+        plt.plot(xh,yh,color="red")
 
     for j in range(m):
         best = points[0] if points[0] != last else points[1] 
@@ -65,6 +99,7 @@ def step_chan(points, m):
                 best = q
         L.append(best)
         last = best
+        plt.plot(last[0], last[1], 'bo')
         if best == p1:
             L.pop()  # usuwamy powtorzony punkt startowy
             return L
