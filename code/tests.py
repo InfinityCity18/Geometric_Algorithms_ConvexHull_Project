b925ef1537a037df7ea09a7356c442fcc7610d13
Jakub <kubawlas1@gmail.com>
2025-12-31 03:23:13 +0100

    added zigzag testset

--- code/tests.py
+++ code/tests.py
@@ -1,7 +1,7 @@
 import numpy as np
 from drawing import draw_points, draw_hull
 
-np.random.seed(19)
+np.random.seed(13)
 
 def generate_uniform_points(left=-100, right=100, n=100):
     return [(np.random.uniform(left, right), np.random.uniform(left, right)) for _ in range(n)]
@@ -13,7 +13,30 @@ def generate_circle_points(O, R, n=100):
         points.append((O[0] + R * np.sin(angle), O[1] + R * np.cos(angle)))
     return points
 
-testset = generate_uniform_points(-10,10,50)
+def generate_zigzag_points(width, height, n, amplitude, period):
+    points = [(np.random.uniform(-width, width), np.random.uniform(-height, height)) for _ in range(n)]
+    n = 2 * width // period
+    for i in range(n):
+        x = -width + i * period
+        y = -height + amplitude * ((-1) ** i)
+        points.append((x, y))
+    for i in range(n):
+        x = -width + i * period
+        y = height + amplitude * ((-1) ** i)
+        points.append((x, y))
+    n = 2 * height // period
+    for i in range(n):
+        x = -width + amplitude * ((-1) ** i)
+        y = -height + i * period
+        points.append((x, y))
+    for i in range(n):
+        x = width + amplitude * ((-1) ** i)
+        y = -height + i * period
+        points.append((x, y))
+    return points
+
+testset = generate_uniform_points(-10,10,100)
+#testset = generate_zigzag_points(100,100,0,10,10)
 from graham import *
 from jarvis import *
 from monochain import *
