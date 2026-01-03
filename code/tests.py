037552d00d214769e7414801ec9dc4be3415405c
Remigiusz Babiarz <rbabiarz@student.agh.edu.pl>
2026-01-03 17:08:19 +0100

    added main.py, needed functionalities to drawing and tests

--- code/tests.py
+++ code/tests.py
@@ -6,14 +6,14 @@ np.random.seed(13)
 def generate_uniform_points(left=-100, right=100, n=100):
     return [(np.random.uniform(left, right), np.random.uniform(left, right)) for _ in range(n)]
 
-def generate_circle_points(O=(0,0), R=10, n=100):
+def generate_circle_points(O=(0,0), R=100, n=100):
     points = []
     for i in range(n):
         angle = np.random.uniform(0, 2*np.pi)
         points.append((O[0] + R * np.sin(angle), O[1] + R * np.cos(angle)))
     return points
 
-def generate_zigzag_points(width=10, height=10, n=100, amplitude=5, period=5):
+def generate_zigzag_points(width=100, height=100, n=100, amplitude=10, period=10):
     points = [(np.random.uniform(-width, width), np.random.uniform(-height, height)) for _ in range(n)]
     n = 2 * width // period
     for i in range(n):
@@ -35,19 +35,9 @@ def generate_zigzag_points(width=10, height=10, n=100, amplitude=5, period=5):
         points.append((x, y))
     return points
 
-#testset = generate_uniform_points(-10,10,100)
-testset = generate_zigzag_points(100,100,0,10,10)
-from graham import *
-from jarvis import *
-from monochain import *
-from quickhull import *
-from divide_and_conquer import *
-from chan import *
-from incremental import *
-print("convex hull points jarvis: ", len(jarvis(testset)))
-print("convex hull points graham: ", len(graham(testset)))
-print("convex hull points incremental: ", len(incremental(testset)))
-print("convex hull points monochain: ", len(monochain(testset)))
-print("convex hull points quickhull: ", len(quickhull(testset)))
-print("convex hull points divide and conquer: ", len(divide_and_conquer(testset)))
-graham_vis(testset)
+def get_generators():
+    gens = []
+    gens.append((generate_uniform_points, "Generuje losowe punkty o współrzędnych z zakresiu [-100,100]"))
+    gens.append((generate_circle_points, "Generuje losowe punkty leżące na kole o promieniu 100"))
+    gens.append((generate_zigzag_points, "Generuje losowe punkty wewnątrz kwadratowego obszaru, które otoczone są obramówką"))
+    return gens
