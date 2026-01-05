eb6fa957c61aa572eb29fc1b8c2687fc051e9dd2
Jakub <kubawlas1@gmail.com>
2026-01-05 18:56:05 +0100

    changed code to src, moved main up, fixed imports

---
+++ src/graham.py
@@ -0,0 +1,94 @@
+from collections import deque
+from numpy import atan2
+eps = 10**-12
+
+def dist(p1,p2):
+    return (p1[0] - p2[0])**2 + (p1[1]-p2[1])**2
+
+def det(a, b, c):       
+     return (b[0]-a[0])*(c[1]-a[1]) - (b[1]-a[1])*(c[0]-a[0])
+
+def graham(points):
+    points = points.copy()
+
+    #znajdowanie najniższego punktu
+    lowest_point = points[0]
+    for p in points:
+        if p[1] == lowest_point[1] and p[0] < lowest_point[0]: lowest_point = p
+        elif p[1] < lowest_point[1]: lowest_point = p
+    points.remove(lowest_point)
+    
+    #sortowanie punktów 
+    points.sort(key=lambda p: (atan2(p[1] - lowest_point[1], p[0] - lowest_point[0]),
+                               dist(p,lowest_point))) 
+
+    #usuwanie punktów współliniowych
+    temp_points = []
+    n = len(points)
+    i = 0
+    while i < n:
+        j = i
+        while j + 1 < n and abs(det(lowest_point, points[j], points[j+1])) <= eps:
+                   j += 1
+        temp_points.append(points[j])
+        i = j + 1
+
+    points = temp_points
+
+    #główna pętla
+    hull = [lowest_point]
+    for p in points:
+        while len(hull) >= 2 and det(hull[-2], hull[-1], p) <= eps:
+            hull.pop()
+        hull.append(p)
+    
+    return hull
+
+
+def graham_vis(points,title ="Graham",path=None):
+    from drawing import Visualizer
+    viz = Visualizer(f"{title} n = {len(points)}")
+    viz.add_permament([("points", "gray", points)])
+    num_frames = 0
+
+
+    points = points.copy()
+    lowest_point = points[0]
+    for p in points:
+        if p[1] == lowest_point[1] and p[0] < lowest_point[0]: lowest_point = p
+        elif p[1] < lowest_point[1]: lowest_point = p
+    points.remove(lowest_point)
+    
+    points.sort(key=lambda p: (atan2(p[1] - lowest_point[1], p[0] - lowest_point[0]),
+                               dist(p,lowest_point))) 
+    temp_points = []
+    n = len(points)
+    i = 0
+    while i < n:
+        j = i
+        while j + 1 < n and abs(det(lowest_point, points[j], points[j+1])) <= eps:
+                   j += 1
+        temp_points.append(points[j])
+        i = j + 1
+
+    points = temp_points
+
+    hull = [lowest_point]
+    
+    def snap(current):
+        nonlocal num_frames
+        num_frames+=1
+        frame = []
+        frame.append(("polygon_open", "red", hull.copy()))
+        if(current): frame[0][2].append(current)
+        viz.add_frame(frame)
+    snap(None)
+    for p in points:
+        while len(hull) >= 2 and det(hull[-2], hull[-1], p) <= eps:
+            hull.pop()
+            snap(p)
+        hull.append(p)
+        snap(None)
+
+    viz.add_frame([("polygon", "red", hull.copy()), ("points", "red", hull.copy(), 4)])
+    viz.draw_animation(10000/num_frames, path)    
---
+++ src/tests.py
@@ -0,0 +1,80 @@
+import numpy as np
+import time
+from src.drawing import *
+
+np.random.seed(13)
+
+def generate_uniform_points(left=-100, right=100, n=100):
+    return [(np.random.uniform(left, right), np.random.uniform(left, right)) for _ in range(n)]
+
+def generate_circle_points(O=(0,0), R=100, n=100):
+    points = []
+    for i in range(n):
+        angle = np.random.uniform(0, 2*np.pi)
+        points.append((O[0] + R * np.sin(angle), O[1] + R * np.cos(angle)))
+    return points
+
+def generate_zigzag_points(width=100, height=100, n=100, amplitude=10, period=10):
+    points = [(np.random.uniform(-width, width), np.random.uniform(-height, height)) for _ in range(n)]
+    n = 2 * width // period
+    for i in range(n):
+        x = -width + i * period
+        y = -height + amplitude * ((-1) ** i)
+        points.append((x, y))
+    for i in range(n):
+        x = -width + i * period
+        y = height + amplitude * ((-1) ** i)
+        points.append((x, y))
+    n = 2 * height // period
+    for i in range(n):
+        x = -width + amplitude * ((-1) ** i)
+        y = -height + i * period
+        points.append((x, y))
+    for i in range(n):
+        x = width + amplitude * ((-1) ** i)
+        y = -height + i * period
+        points.append((x, y))
+    return points
+
+def get_generators():
+    gens = []
+    gens.append((generate_uniform_points, "Generuje losowe punkty o współrzędnych z zakresiu [-100,100]"))
+    gens.append((generate_circle_points, "Generuje losowe punkty leżące na kole o promieniu 100"))
+    gens.append((generate_zigzag_points, "Generuje losowe punkty wewnątrz kwadratowego obszaru, które otoczone są obramówką"))
+    return gens
+
+def benchmark_convex_hull(convex_hull_algs, generator, ns, **kwargs,):
+    """
+    Generuje punkty i mierzy czas wykonania otoczki wypukłej dla różnych n.
+    
+    :param convex_hull: lista funkcji otoczki
+    :param generator: funkcja generująca punkty, np. generate_uniform_points
+    :param ns: lista liczb punktów [100, 500, 1000, ...]
+    :param kwargs: dodatkowe argumenty do generatora
+    """
+    fig, ax = plt.subplots(figsize=(7,5))
+
+    pts_array = [generator(n=n, **kwargs) for n in ns]
+    
+    for n in ns:
+        print(n, end='\t')
+    print("n")
+    for alg in convex_hull_algs:
+        times = []
+        for pts in pts_array:
+            start = time.perf_counter_ns()
+            alg(pts)
+            elapsed = (time.perf_counter_ns() - start) / 1e9
+            times.append(elapsed)
+            print("{:.2f}".format(elapsed), end='\t')
+        print(f"czas {alg.__name__}", end='\t')
+        print()
+        ax.plot(ns, times, 'o-', label=alg.__name__)
+        
+    ax.set_xlabel('Liczba punktów n')
+    ax.set_ylabel('Czas wykonania [ms]')
+    ax.set_title(f'Czasy wykonania dla generatora {generator.__name__}')
+    ax.grid(True)
+    ax.legend()
+    plt.show()
+
