f47010ef16bcea4cf4ce79c9fc97ecc59f535db3
Remigiusz Babiarz <rbabiarz@student.agh.edu.pl>
2026-01-06 11:52:03 +0100

    fixed divide and conquer

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ print("[2] - analiza dla generatora")
 inp = input_range(1,2)
 if inp == 2:
     g = select_generator()
-    ns = [10, 100, 500, 1000, 2500, 5000, 7500, 10000]
+    ns = list(range(10000, 200000, 10000))
     if g.__name__ == 'generate_circle_points': 
         ns = [10, 50, 100, 250, 500, 750, 1000]
 
